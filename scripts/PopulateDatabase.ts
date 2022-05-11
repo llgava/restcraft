80a21719344ec270c48d2db9c7f14389eaffb5f6
llgava <llgavamt@gmail.com>
2022-05-11 14:07:58 -0400

    Remove extra comment

--- scripts/PopulateDatabase.ts
+++ scripts/PopulateDatabase.ts
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import MinecraftDataManager from '../src/utils/MinecraftDataManager';
 import { IBlock } from '../src/typings/IBlocks';
-import { MinecraftBlock } from '../src/models/MinecraftBlock';
 
 MinecraftDataManager.blocks.forEach(block => {
   axios.post('http://localhost:3000/api/blocks', <IBlock>{
     id: block.id,
     name: block.name,
     has_variations: block.metadata.length > 0 ? true : false,
-    texture_data: block.texture_data, // TODO: change in the future
+    texture_data: block.texture_data,
     metadata: block.metadata
   });
 });
